d00b55ee25d999fee75acb415ecc1da07a3c9d0b
Feridali <ferid62@hotmail.com>
2024-05-20 21:18:22 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Show from "./pages/Show";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/:id" element={<Show />} />
+    </Routes>
+  </BrowserRouter>
+);
